8944f345b930c7fc3e2580a54cc7693f4bfa5d8c
ipscoder <psawant0015@gmail.com>
2024-08-18 13:31:03 +0530

    text reveal

--- src/pages/gsap/GsapAnimation.jsx
+++ src/pages/gsap/GsapAnimation.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import GsapTimer from './timer/GsapTimer'
+import GsapScroll from './scroll/GsapScroll'
+import TextReveal from './text-reveal/TextReval'
 
 const GsapAnimation = () => {
   return (
     <>
     <GsapTimer/>
+    <GsapScroll/>
+    <TextReveal/>
     </>
   )
 }
